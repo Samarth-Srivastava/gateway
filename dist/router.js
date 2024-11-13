cabc1868ee7655f9ee1a0f3511cc4b07605bec91
Samarth Srivastava <samarth.kumar.srivastava@gmail.com>
2024-11-13 22:00:26 +0530

    added env

--- dist/router.js
+++ dist/router.js
@@ -7,41 +7,26 @@ const express_1 = __importDefault(require("express"));
 var router = express_1.default.Router();
 const apiAdapter_1 = __importDefault(require("./apiAdapter"));
 const dbModel_1 = __importDefault(require("./dbModel"));
-var routes = [
-    {
-        "TENANT": "Jaadu",
-        "METHOD": "get",
-        "UPSTREAM-PROTOCOL": "http",
-        "UPSTREAM-DOMAIN": "localhost",
-        "UPSTREAM-PORT": 3031,
-        "UPSTREAM-ENDPOINT": "/api/order",
-        "DOWNSTREAM-ENDPOINT": "/feeds"
-    },
-    {
-        "TENANT": "Jaadu",
-        "METHOD": "post",
-        "UPSTREAM-PROTOCOL": "http",
-        "UPSTREAM-DOMAIN": "localhost",
-        "UPSTREAM-PORT": 3031,
-        "UPSTREAM-ENDPOINT": "/api/productOrderScreenViewList",
-        "DOWNSTREAM-ENDPOINT": "/feeder"
-    }
-];
 router.use((req, res, next) => {
     var _a;
     console.log("Called: ", req.path);
     var getTenant = (_a = req.headers.tenant) !== null && _a !== void 0 ? _a : null;
     if (getTenant) {
         var route;
-        dbModel_1.default.find({ TENANT: getTenant }).then(allroutesForTenant => {
-            route = allroutesForTenant.filter(a => a.DOWNSTREAM_ENDPOINT == req.path)[0];
-        }).then(abc => {
+        dbModel_1.default
+            .find({ TENANT: getTenant })
+            .then((allroutesForTenant) => {
+            route = allroutesForTenant.filter((a) => a.DOWNSTREAM_ENDPOINT == req.path)[0];
+        })
+            .then((abc) => {
             if (route && route.METHOD == "get") {
-                var port = route.UPSTREAM_PORT ? ":" + route.UPSTREAM_PORT : '';
-                var BASE_URL = route.UPSTREAM_PROTOCOL + '://' + route.UPSTREAM_DOMAIN + port;
+                var port = route.UPSTREAM_PORT ? ":" + route.UPSTREAM_PORT : "";
+                var BASE_URL = route.UPSTREAM_PROTOCOL + "://" + route.UPSTREAM_DOMAIN + port;
                 const api = (0, apiAdapter_1.default)(BASE_URL);
                 var url = route.UPSTREAM_ENDPOINT;
-                api.get(url).then((resp) => {
+                api
+                    .get(url)
+                    .then((resp) => {
                     res.send(resp.data);
                 })
                     .catch((ex) => {
@@ -49,12 +34,14 @@ router.use((req, res, next) => {
                 });
             }
             else if (route && route.METHOD == "post") {
-                var port = route.UPSTREAM_PORT ? ":" + route.UPSTREAM_PORT : '';
-                var BASE_URL = route.UPSTREAM_PROTOCOL + '://' + route.UPSTREAM_DOMAIN + port;
+                var port = route.UPSTREAM_PORT ? ":" + route.UPSTREAM_PORT : "";
+                var BASE_URL = route.UPSTREAM_PROTOCOL + "://" + route.UPSTREAM_DOMAIN + port;
                 const api = (0, apiAdapter_1.default)(BASE_URL);
                 var url = route.UPSTREAM_ENDPOINT;
                 console.log(req);
-                api.post(url, req.body).then((resp) => {
+                api
+                    .post(url, req.body)
+                    .then((resp) => {
                     res.send(resp.data);
                 })
                     .catch((ex) => {
